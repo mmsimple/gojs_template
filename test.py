59b9f16e30b6f19bbfe5a5c0650a99cc8b7139fb
simple <792378197@qq.com>
2022-10-27 22:43:12 +0800

    add

---
+++ test.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+
+driver.get("file:///Users/simple/Desktop/MG/gojs_template/C1.html")
+# driver.get('/Users/simple/Desktop/MG/gojs_template/C1.html')
